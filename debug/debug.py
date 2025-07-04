7afddbc176d633bf98359b04237fe621430e1d06
Marius Briscut <marius.briscut5@gmail.com>
2025-07-04 15:44:35 -0400

    framework update

---
+++ debug/debug.py
@@ -0,0 +1,7 @@
+# for unit testing and debugging code
+
+import unittest
+from time import sleep
+
+if __name__ == "__main__":
+    print("It works!")
--- resources/debug.py
+++
@@ -1,4 +0,0 @@
-# for unit testing and debugging code snippets
-
-if __name__ == "__main__":
-    print("It works!")
